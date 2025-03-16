5a39f5f1621adb32881f65d2f09a2e5c7e76f5e9
Jane Allinger <janeallinger@Janes-Air.attlocal.net>
2025-03-16 13:35:24 -0500

    project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+.page {
+  font-family: 'Arial', sans-serif;
+  background-color: #f8f8f8;
+  margin: 0;
+  padding: 0;
+}
+</style>
