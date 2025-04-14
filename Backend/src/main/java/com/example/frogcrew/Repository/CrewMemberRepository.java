d406bdd2b39af6f0d40458111369b030705ef410
ArsceneR <rubayitaarscene@gmail.com>
2025-04-14 00:50:38 -0500

    created repository for crew members

--- Backend/src/main/java/com/example/frogcrew/Repository/CrewMemberRepository.java
+++ Backend/src/main/java/com/example/frogcrew/Repository/CrewMemberRepository.java
@@ -3,7 +3,9 @@ package com.example.frogcrew.Repository;
 import com.example.frogcrew.Model.CrewMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import java.util.UUID;
 
 public interface CrewMemberRepository extends JpaRepository<CrewMember, UUID> {
+    //will implement custom Spring Data methods here.. for now most of the ones i can think of are inherited from JPA
 }
